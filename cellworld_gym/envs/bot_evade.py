3b6443715d189831f7c62040471e6641662028f2
German Espinosa <germanespinosa@gmail.com>
2024-04-18 11:31:54 -0500

    added BotEvadeReward

--- cellworld_gym/envs/bot_evade.py
+++ cellworld_gym/envs/bot_evade.py
@@ -31,6 +31,22 @@ class BotEvadeObservation(typing.List[float]):
         list.__setitem__(self, field.value, value)
 
 
+class BotEvadeReward(object):
+    def __init__(self,
+                 reward_structure: dict):
+        self.reward_structure = reward_structure
+
+    def __call__(self, observation: typing.List[float]) -> float:
+        reward = 0.0
+        for field, multiplier in self.reward_structure.items():
+            offset = 0
+            if isinstance(multiplier, list):
+                offset = multiplier[1]
+                multiplier = multiplier[0]
+            reward += offset + multiplier * observation[BotEvadeReward.Field[field].value]
+        return reward
+
+
 class BotEvade(Env):
     def __init__(self,
                  world_name: str,
