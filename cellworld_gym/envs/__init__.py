4e4904b4f672cda080b5a290b88f79fddaac17a9
German Espinosa <germanespinosa@gmail.com>
2024-04-26 15:33:33 -0500

    registered dualevadeenv

--- cellworld_gym/envs/__init__.py
+++ cellworld_gym/envs/__init__.py
@@ -1,6 +1,7 @@
 from gymnasium.envs.registration import register
 from .bot_evade import BotEvadeEnv, BotEvadeObservation
 from .oasis import OasisEnv, OasisObservation
+from .dual_evade import DualEvadeEnv, DualEvadeObservation
 
 register(
     id='CellworldBotEvade-v0',
@@ -10,4 +11,9 @@ register(
 register(
     id='CellworldOasis-v0',
     entry_point='cellworld_gym.envs:OasisEnv'
+)
+
+register(
+    id='CellworldDualEvade-v0',
+    entry_point='cellworld_gym.envs:DualEvadeEnv'
 )
